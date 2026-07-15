a2e0e1d118998e57cc753fd0e2979ea12275bbfa
epsilon003 <abhimantrsingh@gmail.com>
2026-07-15 19:23:46 +0530

    Privacy Policy

---
+++ src/privacy/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "../options/options.css";
+import "./privacy.css";
+import Privacy from "./Privacy";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Privacy />
+  </StrictMode>
+);
